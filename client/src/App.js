241e50889bab18c0150a81fcd350a17116f478c7
Justin Bass <justinbass6036@gmail.com>
2024-04-16 22:10:59 -0400

    Set up route logic in app.js and imported my Provider Components into app.js to set up global state for my models.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { UserProvider } from "./context/user"
+import { ProductProvider } from "./context/product";
+import { OrderableProvider } from "./context/orderable";
+
+
+import './App.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <UserProvider>
+      <ProductProvider>
+        <OrderableProvider>
+                <div className="App">
+                  <Routes>
+                    <Route path="/" element={''} />
+                  </Routes>
+                </div>
+        </OrderableProvider>
+       </ProductProvider>
+     </UserProvider>
   );
 }
 
